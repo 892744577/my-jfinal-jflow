6e6e2b3c60ff115938481e9b1a9a6ea976f84133
roman <892744577@qq.com>
2020-05-20 09:44:56 +0800

    feat:合成海报

--- src/main/java/com/kakarote/crm9/erp/sms/controller/SmsController.java
+++ src/main/java/com/kakarote/crm9/erp/sms/controller/SmsController.java
@@ -1,11 +1,19 @@
 package com.kakarote.crm9.erp.sms.controller;
 
+import com.google.zxing.WriterException;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
 import com.jfinal.core.paragetter.Para;
 import com.kakarote.crm9.erp.sms.entity.LoginRequestDto;
+import com.kakarote.crm9.erp.sms.entity.PictureRequestDto;
 import com.kakarote.crm9.erp.sms.service.SmsService;
+import com.kakarote.crm9.utils.PictureUtil;
 import com.kakarote.crm9.utils.R;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class SmsController extends Controller {
 
@@ -24,8 +32,27 @@ public class SmsController extends Controller {
         renderJson(R.ok().put("openId","xxx").put("code","000000"));
     }
 
+    /**
+     * 根据手机号获取验证码
+     * @author tangmanrong
+     * @param loginRequestDto
+     */
     public void getSmsByMobile(@Para("") LoginRequestDto loginRequestDto){
 
         renderJson(R.ok().put("验证码",smsService.getSmsByMobile(loginRequestDto)).put("code","000000"));
+    }
+
+    public void createPlayBill(@Para("") PictureRequestDto pictureRequestDto){
+        try {
+            BufferedImage code = PictureUtil.createImage(pictureRequestDto.getContent(), null, false);
+            BufferedImage big = PictureUtil.combineCodeAndPicToFile(pictureRequestDto.getBackPicPath(), code);
+            String suffix = pictureRequestDto.getBackPicPath().substring(pictureRequestDto.getBackPicPath().lastIndexOf(".") + 1);
+            ImageIO.write(big,suffix , new File(pictureRequestDto.getOutPicPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/com/kakarote/crm9/erp/sms/entity/PictureRequestDto.java
@@ -0,0 +1,22 @@
+package com.kakarote.crm9.erp.sms.entity;
+
+import lombok.Data;
+
+@Data
+public class PictureRequestDto {
+
+    /**
+     * 二维码内容
+     */
+    private String content;
+
+    /**
+     * os系统物理输入路径且后缀为png
+     */
+    private String backPicPath;
+    /**
+     * os系统物理输出路径且后缀为jpg
+     */
+    private String outPicPath;
+
+}
