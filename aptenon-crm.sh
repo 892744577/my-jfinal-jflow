f3efd0fcc7cd283c140b21ae94b3c7ed660dc5b8
roman <892744577@qq.com>
2020-05-28 13:29:27 +0800

    feat:添加远程调试

--- aptenon-crm.sh
+++ aptenon-crm.sh
@@ -14,7 +14,7 @@ fi
 # Java 命令行参数，根据需要开启下面的配置，改成自己需要的，注意等号前后不能有空格
 # JAVA_OPTS="-Xms256m -Xmx1024m -Dundertow.port=80 -Dundertow.host=0.0.0.0"
 # JAVA_OPTS="-Dundertow.port=80 -Dundertow.host=0.0.0.0"
-
+# JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8240"
 # 生成 class path 值
 
 APP_BASE_PATH=$(cd `dirname $0`; pwd)
