02f3e207b6cfc9940de10c70bb040388df5b24a7
roman <892744577@qq.com>
2020-06-03 10:38:25 +0800

    feat:微信中台获取数据接口

--- src/main/java/com/kakarote/crm9/erp/yzj/common/YzjRouter.java
+++ src/main/java/com/kakarote/crm9/erp/yzj/common/YzjRouter.java
@@ -1,6 +1,7 @@
 package com.kakarote.crm9.erp.yzj.common;
 
 import com.jfinal.config.Routes;
+import com.kakarote.crm9.erp.yzj.controller.WxMiddleWareController;
 import com.kakarote.crm9.erp.yzj.controller.YzjController;
 
 public class YzjRouter extends Routes {
@@ -8,5 +9,6 @@ public class YzjRouter extends Routes {
     public void config() {
         addInterceptor(new YzjInterceptor());
         add("/yzj", YzjController.class);
+        add("/wxMiddleWare", WxMiddleWareController.class);
     }
 }
---
+++ src/main/java/com/kakarote/crm9/erp/yzj/controller/WxMiddleWareController.java
@@ -0,0 +1,10 @@
+package com.kakarote.crm9.erp.yzj.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * 微信中台，根据手机号获取公众号openid
+ */
+public class WxMiddleWareController extends Controller {
+
+}
