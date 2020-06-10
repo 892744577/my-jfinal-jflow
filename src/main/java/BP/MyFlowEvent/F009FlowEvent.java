84553d9e1542c3da05d0afa8a6faff11db903fa5
roman <892744577@qq.com>
2020-06-10 16:13:00 +0800

    feat:009流程设计

---
+++ src/main/java/BP/MyFlowEvent/F009FlowEvent.java
@@ -0,0 +1,17 @@
+package BP.MyFlowEvent;
+
+import BP.WF.FlowEventBase;
+
+public class F009FlowEvent extends FlowEventBase {
+    @Override
+    public String getFlowMark() {
+        return "aptenon_wf_gdsq";
+    }
+
+    @Override
+    public String SendSuccess(){
+
+        return null;
+    }
+
+}
