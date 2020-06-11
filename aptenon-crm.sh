39632dd4fd7e7a9232d838e104e499b6aa41e289
roman <892744577@qq.com>
2020-06-11 11:29:22 +0800

    feat:添加调试命令

--- aptenon-crm.sh
+++ aptenon-crm.sh
@@ -5,8 +5,8 @@ MAIN_CLASS=com.kakarote.crm9.Application
 
 COMMAND="$1"
 
-if [[ "$COMMAND" != "start" ]] && [[ "$COMMAND" != "stop" ]] && [[ "$COMMAND" != "restart" ]]; then
-	echo "Usage: $0 start | stop | restart"
+if [[ "$COMMAND" != "start" ]] && [[ "$COMMAND" != "stop" ]] && [[ "$COMMAND" != "restart" ]] && [[ "$COMMAND" != "debug" ]]; then
+	echo "Usage: $0 start | stop | restart | debug"
 	exit 0
 fi
 
@@ -14,12 +14,17 @@ fi
 # Java 命令行参数，根据需要开启下面的配置，改成自己需要的，注意等号前后不能有空格
 # JAVA_OPTS="-Xms256m -Xmx1024m -Dundertow.port=80 -Dundertow.host=0.0.0.0"
 # JAVA_OPTS="-Dundertow.port=80 -Dundertow.host=0.0.0.0"
-# JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8240"
+JAVA_OPTS_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=8240"
 # 生成 class path 值
 
 APP_BASE_PATH=$(cd `dirname $0`; pwd)
 
 CP=${APP_BASE_PATH}/config:${APP_BASE_PATH}/lib/*
+
+function debug()
+{
+    nohup java -Xverify:none ${JAVA_OPTS_DEBUG} -cp ${CP} ${MAIN_CLASS} > ./output.log &
+}
 
 function start()
 {
@@ -35,6 +40,8 @@ if [[ "$COMMAND" == "start" ]]; then
 	start
 elif [[ "$COMMAND" == "stop" ]]; then
     stop
+elif [[ "$COMMAND" == "debug" ]]; then
+    debug
 else
     stop
     start
