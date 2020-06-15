f48ce34963bbef5cafa25f1b7a657fc71afaf4ba
roman <892744577@qq.com>
2020-06-15 14:19:02 +0800

    feat:言而有信api

--- src/main/java/com/kakarote/crm9/erp/yzj/common/YzjRouter.java
+++ src/main/java/com/kakarote/crm9/erp/yzj/common/YzjRouter.java
@@ -2,6 +2,7 @@ package com.kakarote.crm9.erp.yzj.common;
 
 import com.jfinal.config.Routes;
 import com.kakarote.crm9.erp.yzj.controller.WxMiddleWareController;
+import com.kakarote.crm9.erp.yzj.controller.YeyxController;
 import com.kakarote.crm9.erp.yzj.controller.YzjController;
 
 public class YzjRouter extends Routes {
@@ -9,6 +10,7 @@ public class YzjRouter extends Routes {
     public void config() {
         addInterceptor(new YzjInterceptor());
         add("/yzj", YzjController.class);
-        add("/wxMiddleWare", WxMiddleWareController.class);
+        add("/wxMiddleWare", WxMiddleWareController.class); //微信中台
+        add("/yeyx", YeyxController.class); //言而有信订单平台
     }
 }
---
+++ src/main/java/com/kakarote/crm9/erp/yzj/controller/YeyxController.java
@@ -0,0 +1,48 @@
+package com.kakarote.crm9.erp.yzj.controller;
+
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.kakarote.crm9.erp.yzj.service.YeyxService;
+
+/**
+ * 言而有信第三方订单平台接口
+ */
+public class YeyxController extends Controller {
+
+    @Inject
+    private YeyxService yeyxService;
+
+    /**
+     * 改约
+     */
+    public void changeTime(){
+
+    }
+    /**
+     * 派单
+     */
+    public void launchService(){
+
+    }
+    /**
+     * 上门
+     */
+    public void toDoor(){
+
+    }
+
+    /**
+     * 完成
+     */
+    public void complete(){
+
+    }
+
+    /**
+     * 取消
+     */
+    public void cancel(){
+
+    }
+
+}
