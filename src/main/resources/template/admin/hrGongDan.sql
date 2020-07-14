f40cec477a30d71fb6ea0b1e21f87d9c8f1e08f4
IT-KD <kidda_wang@163.com>
2020-07-14 18:09:58 +0800

    工单信息接口代码提交

--- src/main/resources/template/admin/hrGongDan.sql
+++ src/main/resources/template/admin/hrGongDan.sql
@@ -19,7 +19,7 @@
       #end
       order by a.rdt desc
   #end
-  #sql ("getHrhrGongDanByOrderId")
-    select * from hr_gongdan a where a.orderId=?
+  #sql ("getHrGongDanByOrderId")
+    select * from hr_gongdan a where a.orderId=#para(orderId)
   #end
 #end
