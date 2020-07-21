4d7f70a88d14f284c533d1b5ff8dca3d120b4301
roman <892744577@qq.com>
2020-07-21 14:38:26 +0800

    feat:查询连接待办表会出现多条数据，因为会有多人相同流程的不同待办

--- src/main/resources/template/admin/hrGongDan.sql
+++ src/main/resources/template/admin/hrGongDan.sql
@@ -1,7 +1,6 @@
 #namespace("admin.hrGongDan")
   #sql ("getHrGongDanPageList")
     select a.*,b.FK_Node,b.NodeName from hr_gongdan a left join wf_generworkflow b on a.oid = b.WorkID
-    LEFT JOIN wf_generworkerlist c ON c.WorkID = b.WorkID AND c.FK_Node = b.FK_Node
     where 1=1 and  b.FK_Node != 901
       #if(search)
       and (a.serviceNo like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.contactName like CONCAT('%',#para(search),'%') or a.telephone like CONCAT('%',#para(search),'%'))
