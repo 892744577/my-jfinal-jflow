9ee8c00ae5ccef750cd0b254f9df470fb4826ada
roman <892744577@qq.com>
2020-07-30 11:22:42 +0800

    feat:工单增加按openid查询

--- src/main/resources/template/admin/hrGongDanRepair.sql
+++ src/main/resources/template/admin/hrGongDanRepair.sql
@@ -3,11 +3,12 @@
     select count(*) from hr_gongdan_repair a where DATE_FORMAT(a.create_time, '%Y%m%d') = #para(today)
   #end
   #sql ("queryPageList")
-    select a.*,(case when a.deal='2' then '2' when (SELECT COUNT(*)>0 FROM hr_gongdan_log t1
-    WHERE t1.preServiceNo=a.orderNumber)=1 then '1' else '0' end) deal_1 from hr_gongdan_repair a
-    where 1=1
+    select a.*,(case when a.deal='2' then '2' when (SELECT COUNT(*)>0 FROM hr_gongdan_log t1 WHERE t1.preServiceNo=a.orderNumber)=1 then '1' else '0' end) deal_1 from hr_gongdan_repair a where 1=1
     #if(search)
       and (a.contact like CONCAT('%',#para(search),'%') or a.phone like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.remark like CONCAT('%',#para(search),'%'))
+    #end
+    #if(open_id)
+      and a.open_id = #para(open_id)
     #end
     order by a.create_time desc
   #end
