dc432fbc7a28bc96e487f7aabdf51c8d55067579
roman <892744577@qq.com>
2020-07-31 14:23:31 +0800

    feat:open_id来搜索数据

--- src/main/resources/template/admin/hrGongDanBook.sql
+++ src/main/resources/template/admin/hrGongDanBook.sql
@@ -5,7 +5,7 @@
   #sql ("queryPageList")
     select a.*,(case when a.deal='2' then '2' when (SELECT COUNT(*)>0 FROM hr_gongdan_log t1 WHERE t1.preServiceNo=a.orderNumber)=1 then '1' else '0' end) deal_1 from hr_gongdan_book a where 1=1
     #if(search)
-      and (a.contact like CONCAT('%',#para(search),'%') or a.phone like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.remark like CONCAT('%',#para(search),'%'))
+      and (a.orderNumber like CONCAT('%',#para(search),'%') or a.contact like CONCAT('%',#para(search),'%') or a.phone like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.remark like CONCAT('%',#para(search),'%'))
     #end
     #if(open_id)
       and a.open_id = #para(open_id)
