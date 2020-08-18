ddccf53bd930984f29a88b6c238c0b8e579250c8
IT-KD <kidda_wang@163.com>
2020-08-18 16:45:57 +0800

    进行小程序信息推送代码提交

--- src/main/java/BP/MyFlowEvent/F008FlowEvent.java
+++ src/main/java/BP/MyFlowEvent/F008FlowEvent.java
@@ -8,7 +8,6 @@ import com.kakarote.crm9.erp.admin.entity.Regist;
 import com.kakarote.crm9.erp.admin.service.PortEmpService;
 import com.kakarote.crm9.erp.wx.util.MaUtil;
 import com.kakarote.crm9.erp.wx.vo.MaReq;
-import com.kakarote.crm9.utils.R;
 
 import java.util.Date;
 
@@ -31,31 +30,7 @@ public class F008FlowEvent extends FlowEventBase {
             Aop.get(PortEmpService.class).savePortEmp(regist);
 
             //进行小程序信息推送
-            MaReq maReq = new MaReq();
-            maReq.setTouser((String) this.getSysPara().get("appOpenId"));
-            maReq.setTemplate_id("yYVbL8yALguI-hXURqaNkCBosW2T5WEmyTVO0Bu04Kk");
-            maReq.setPage("pages/index/index");
-            maReq.setMiniprogram_state("formal");
-            JSONObject jsonObject=new JSONObject();
-            jsonObject.put("character_string1",new JSONObject().fluentPut("value",(String) this.getSysPara().get("ShouJiHaoMa")));
-            jsonObject.put("name2",new JSONObject().fluentPut("value",(String) this.getSysPara().get("XingMing")));
-            jsonObject.put("date3",new JSONObject().fluentPut("value", DateUtil.formatDateTime(new Date())));
-
-            maReq.setData(jsonObject.toJSONString());
-
-            String sb= MaUtil.ResponseMsg(maReq.getTouser(),maReq.getTemplate_id(),maReq.getPage(),maReq.getData(),maReq.getMiniprogram_state());
-
-            try {
-                boolean IsSend = MaUtil.PostMaMsg(sb);
-
-                if (IsSend == true){
-                    System.out.println("推送小程序信息成功!"+maReq.getTemplate_id());
-                }else {
-                    System.out.println("推送小程序信息失败!"+maReq.getTemplate_id());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            sendMaMsg();
 
         }else if ("2".equals(type)) {
 
@@ -67,35 +42,45 @@ public class F008FlowEvent extends FlowEventBase {
             Aop.get(PortEmpService.class).savePortEmpStaff(regist);
 
             //进行小程序信息推送
-            MaReq maReq = new MaReq();
-            maReq.setTouser((String) this.getSysPara().get("appOpenId"));
-            maReq.setTemplate_id("yYVbL8yALguI-hXURqaNkCBosW2T5WEmyTVO0Bu04Kk");
-            maReq.setPage("pages/index/index");
-            maReq.setMiniprogram_state("formal");
-            JSONObject jsonObject=new JSONObject();
-            jsonObject.put("character_string1",new JSONObject().fluentPut("value",(String) this.getSysPara().get("ShouJiHaoMa")));
-            jsonObject.put("name2",new JSONObject().fluentPut("value",(String) this.getSysPara().get("XingMing")));
-            jsonObject.put("date3",new JSONObject().fluentPut("value", DateUtil.formatDateTime(new Date())));
-
-            maReq.setData(jsonObject.toJSONString());
-
-            String sb= MaUtil.ResponseMsg(maReq.getTouser(),maReq.getTemplate_id(),maReq.getPage(),maReq.getData(),maReq.getMiniprogram_state());
-
-            try {
-                boolean IsSend = MaUtil.PostMaMsg(sb);
-
-                if (IsSend == true){
-                    System.out.println("推送小程序信息成功!"+maReq.getTemplate_id());
-                }else {
-                    System.out.println("推送小程序信息失败!"+maReq.getTemplate_id());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            sendMaMsg();
 
         }
 
         return "F008FlowEvent注册审核流程结束";
+    }
+
+    /*
+     * @Description //进行小程序信息推送
+     * @Author wangkaida
+     * @Date 16:38 2020/8/18
+     **/
+    public void sendMaMsg() {
+        //进行小程序信息推送
+        MaReq maReq = new MaReq();
+        maReq.setTouser((String) this.getSysPara().get("appOpenId"));
+        maReq.setTemplate_id("yYVbL8yALguI-hXURqaNkCBosW2T5WEmyTVO0Bu04Kk");
+        maReq.setPage("pages/index/index");
+        maReq.setMiniprogram_state("formal");
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("character_string1",new JSONObject().fluentPut("value",(String) this.getSysPara().get("ShouJiHaoMa")));
+        jsonObject.put("name2",new JSONObject().fluentPut("value",(String) this.getSysPara().get("XingMing")));
+        jsonObject.put("date3",new JSONObject().fluentPut("value", DateUtil.formatDateTime(new Date())));
+
+        maReq.setData(jsonObject.toJSONString());
+
+        String sb= MaUtil.ResponseMsg(maReq.getTouser(),maReq.getTemplate_id(),maReq.getPage(),maReq.getData(),maReq.getMiniprogram_state());
+
+        try {
+            boolean IsSend = MaUtil.PostMaMsg(sb);
+
+            if (IsSend == true){
+                System.out.println("推送小程序信息成功!"+maReq.getTemplate_id());
+            }else {
+                System.out.println("推送小程序信息失败!"+maReq.getTemplate_id());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
