fe1fe37dc5a1c3e4e9196ecbbcf1fb6533625867
roman <892744577@qq.com>
2020-08-24 13:36:09 +0800

    bug:左木鸟日志查询

--- src/main/resources/template/admin/hrGongDanZmnLog.sql
+++ src/main/resources/template/admin/hrGongDanZmnLog.sql
@@ -3,7 +3,7 @@
     select * from hr_gongdan_zmn_log a
     where 1=1
     #if(search)
-      and (a.thirdOrderId like CONCAT('%',#para(search),'%'))
+      and (a.thirdOrderId like CONCAT('%',#para(search),'-','%'))
     #end
     order by a.optTime desc
   #end
