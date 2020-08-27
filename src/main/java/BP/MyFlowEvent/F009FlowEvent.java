3507faab2c77b3d24f36b5bc4612312d15e59884
roman <892744577@qq.com>
2020-08-27 09:40:54 +0800

    feat:啄木鸟单号当天查不到数据默认为0

--- src/main/java/BP/MyFlowEvent/F009FlowEvent.java
+++ src/main/java/BP/MyFlowEvent/F009FlowEvent.java
@@ -53,7 +53,7 @@ public class F009FlowEvent extends FlowEventBase {
                     String serviceSegmentation = this.getSysPara().get("serviceSegmentation") == null ? "" : this.getSysPara().get("serviceSegmentation").toString(); //安装细分L--晾衣机，S-锁 ，D--门
                     //计算hr_gongdan表数据条数
                     String dateTime = DateUtil.changeDateTOStr2(new Date());
-                    int totalNum = Db.queryInt("select MAX(SUBSTRING(serviceNo,-4)) + 1 from hr_gongdan t where DATE_FORMAT(t.rdt,'%Y%m%d') ="+ dateTime );
+                    int totalNum = Db.queryInt("select IFNULL(MAX(SUBSTRING(serviceNo,-4)) + 1,0) from hr_gongdan t where DATE_FORMAT(t.rdt,'%Y%m%d') ="+ dateTime );
                     String serialNum = (new DecimalFormat("0000")).format(totalNum);//流水号格式化
                     serviceNo = serviceSystem + serviceSegmentation + serviceType + dateTime + serialNum; //服务单编号
                     Row row = this.HisEn.getRow();
