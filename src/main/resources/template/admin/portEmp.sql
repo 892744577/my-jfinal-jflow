c618b3f8862f38949112639411355311cc3f98ba
roman <892744577@qq.com>
2020-09-08 11:21:07 +0800

    bug:查询对应权限组人员，按id

--- src/main/resources/template/admin/portEmp.sql
+++ src/main/resources/template/admin/portEmp.sql
@@ -6,6 +6,6 @@
     SELECT d.* FROM port_emp d
   #end
   #sql ("queryAfterSalePortEmpList")
-    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where c.Name = '客服组'
+    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where b.FK_Group = '02'
   #end
 #end
