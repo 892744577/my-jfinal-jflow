237fd8195a921af8706fb6e1cb41a1c8af554110
roman <892744577@qq.com>
2020-09-10 16:48:11 +0800

    feat:企业微信参数配置优化

--- src/main/resources/template/admin/portEmp.sql
+++ src/main/resources/template/admin/portEmp.sql
@@ -2,10 +2,13 @@
   #sql ("getEmpByWxAppOpenId")
     SELECT d.* FROM port_emp d where d.WxAppOpenId = ?
   #end
+  #sql ("getEmpByTel")
+    SELECT d.* FROM port_emp d where d.tel = ?
+  #end
   #sql ("queryPageList")
     SELECT d.* FROM port_emp d
   #end
   #sql ("queryAfterSalePortEmpList")
-    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where b.FK_Group = '02'
+    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where b.FK_Group in ('02','03')
   #end
 #end
