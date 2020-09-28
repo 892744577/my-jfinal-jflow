308f832df19de37d86ae2ef8e3af22b1a11d9367
roman <892744577@qq.com>
2020-09-28 09:32:33 +0800

    feat:增加账号密码登陆

--- src/main/resources/template/admin/portEmp.sql
+++ src/main/resources/template/admin/portEmp.sql
@@ -2,6 +2,9 @@
   #sql ("getEmpByWxAppOpenId")
     SELECT d.* FROM port_emp d where d.WxAppOpenId = ?
   #end
+  #sql ("getEmpByPass")
+    SELECT d.* FROM port_emp d where d.No = ? and d.Pass = ?
+  #end
   #sql ("getEmpByTel")
     SELECT d.* FROM port_emp d where d.tel = ?
   #end
@@ -9,6 +12,6 @@
     SELECT d.* FROM port_emp d
   #end
   #sql ("queryAfterSalePortEmpList")
-    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where b.FK_Group in ('02','03')
+    select a.* from port_emp a left join gpm_groupemp b on a.No = b.FK_Emp left join gpm_group c on b.FK_Group = c.No where b.FK_Group in ('03')
   #end
 #end
