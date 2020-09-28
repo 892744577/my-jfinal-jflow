9c80028e4dae9957c1e3cdcd89df9642d12de5db
roman <892744577@qq.com>
2020-09-28 09:33:10 +0800

    feat:增加接受人翻译字段

--- src/main/resources/template/admin/hrGongDan.sql
+++ src/main/resources/template/admin/hrGongDan.sql
@@ -37,7 +37,7 @@
   #end
   #sql ("getHrGongDanListPage")
     #(select) FROM (
-    select a.*,b.FK_Node,b.NodeName from hr_gongdan a left join wf_generworkflow b on a.oid = b.WorkID
+    select a.*,b.FK_Node,b.NodeName,c.name acceptorName from hr_gongdan a left join wf_generworkflow b on a.oid = b.WorkID left join port_emp c on a.acceptor = c.no
     where 1=1 and  b.FK_Node != 901
       #if(search)
       and (a.serviceNo like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.contactName like CONCAT('%',#para(search),'%') or a.telephone like CONCAT('%',#para(search),'%'))
@@ -75,7 +75,7 @@
   #end
   #sql ("queryHrGongDanListCount")
     SELECT count(1) FROM (
-    select a.*,b.FK_Node,b.NodeName from hr_gongdan a left join wf_generworkflow b on a.oid = b.WorkID
+    select a.*,b.FK_Node,b.NodeName,c.name acceptorName from hr_gongdan a left join wf_generworkflow b on a.oid = b.WorkID left join port_emp c on a.acceptor = c.no
     where 1=1 and  b.FK_Node != 901
       #if(search)
       and (a.serviceNo like CONCAT('%',#para(search),'%') or a.address like CONCAT('%',#para(search),'%') or a.contactName like CONCAT('%',#para(search),'%') or a.telephone like CONCAT('%',#para(search),'%'))
