d8771340c0a044a09feb589305dd241f883e16b1
roman <892744577@qq.com>
2020-09-30 10:50:56 +0800

    feat:万师傅调用

--- src/main/java/com/kakarote/crm9/erp/yeyx/controller/WanController.java
+++ src/main/java/com/kakarote/crm9/erp/yeyx/controller/WanController.java
@@ -2,12 +2,12 @@ package com.kakarote.crm9.erp.yeyx.controller;
 
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
-import com.kakarote.crm9.erp.yeyx.service.HrGongDanService;
+import com.kakarote.crm9.erp.yeyx.service.WanService;
 
 /**
  * 万师傅接口对接类
  */
 public class WanController extends Controller {
     @Inject
-    private HrGongDanService hrGongDanService;
+    private WanService wanService;
 }
