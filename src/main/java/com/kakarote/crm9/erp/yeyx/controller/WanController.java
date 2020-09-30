eac2e52b30bbe042ab239e8886aa6f5ecbedc8c6
roman <892744577@qq.com>
2020-09-30 10:34:08 +0800

    feat:万师傅调用

---
+++ src/main/java/com/kakarote/crm9/erp/yeyx/controller/WanController.java
@@ -0,0 +1,13 @@
+package com.kakarote.crm9.erp.yeyx.controller;
+
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.kakarote.crm9.erp.yeyx.service.HrGongDanService;
+
+/**
+ * 万师傅接口对接类
+ */
+public class WanController extends Controller {
+    @Inject
+    private HrGongDanService hrGongDanService;
+}
