6cb3a0535c14d732ef7d184f5a155719989f9466
roman <892744577@qq.com>
2020-10-19 11:08:56 +0800

    feat:增加审批备注

--- src/main/java/com/kakarote/crm9/erp/yeyx/entity/vo/HrGongdanRequest.java
+++ src/main/java/com/kakarote/crm9/erp/yeyx/entity/vo/HrGongdanRequest.java
@@ -10,6 +10,10 @@ import lombok.experimental.Accessors;
 public class HrGongdanRequest {
     private Integer oid;
     private String orderId;
-    private String serviceSp;
+    private String serviceSp; //审批状态
+    private String shippingOrderNo; //出货单号
+    private String fuselageCode; //机身码
+    private String serviceSpRemark; //审批备注
+
 
 }
