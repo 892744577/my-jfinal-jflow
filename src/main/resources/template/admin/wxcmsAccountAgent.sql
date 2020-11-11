fe0b3c25e8088072b7f1b063fb93b0a3abaf8104
roman <892744577@qq.com>
2020-11-11 17:37:13 +0800

    feat:查询数据

--- src/main/resources/template/admin/wxcmsAccountAgent.sql
+++ src/main/resources/template/admin/wxcmsAccountAgent.sql
@@ -1,6 +1,8 @@
 #namespace("admin.wxcmsAccountAgent")
   #sql ("queryPageList")
-    SELECT a.* FROM wxcms_account_agent a where 1=1
+    SELECT a.*,b.nick_name,b.gender,b.head_img_url FROM wxcms_account_agent a
+    LEFT JOIN wxcms_account_fans b ON a.open_id = b.open_id
+    where 1=1
     #if(search)
       and (a.province LIKE concat('%',#para(search),'%') or a.city LIKE concat('%',#para(search),'%')
       or a.district LIKE concat('%',#para(search),'%') or a.agentNo like CONCAT('%',#para(search),'%'))
