2b3cb3d95f484d29d49c37877962c50e8eba219d
roman <892744577@qq.com>
2020-11-11 09:41:17 +0800

    bug:shopid已改为shop_id

--- src/main/resources/template/admin/wxcmsAccountQrcodeFans.sql
+++ src/main/resources/template/admin/wxcmsAccountQrcodeFans.sql
@@ -6,6 +6,6 @@
     SELECT d.* FROM wxcms_account_qrcode_fans d where d.fromuser_name = ? and d.event_key = ?
   #end
   #sql ("getFansByShopId")
-    select a.* from wxcms_account_qrcode_fans a left join wxcms_account_shop_qrcode b on a.event_key = b.qrcode_param where b.shopid = ?
+    select a.* from wxcms_account_qrcode_fans a left join wxcms_account_shop_qrcode b on a.event_key = b.qrcode_param where b.shop_id = ?
   #end
 #end
