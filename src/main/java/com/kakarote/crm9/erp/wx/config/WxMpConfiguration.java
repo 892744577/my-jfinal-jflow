843ffa93dc566a0e034e77ca8606a33379f14795
roman <892744577@qq.com>
2020-11-11 09:47:13 +0800

    feat:改为使用线程安全单例模式

--- src/main/java/com/kakarote/crm9/erp/wx/config/WxMpConfiguration.java
+++ src/main/java/com/kakarote/crm9/erp/wx/config/WxMpConfiguration.java
@@ -1,6 +1,5 @@
 package com.kakarote.crm9.erp.wx.config;
 
-
 import BP.Difference.SystemConfig;
 import com.jfinal.aop.Inject;
 import com.kakarote.crm9.erp.wx.mphandler.*;
@@ -61,83 +60,82 @@ public class WxMpConfiguration {
 
     public synchronized WxMpService wxMpService() {
         //单例模式
-        if(this.newRouter!=null){
-            log.info("service不为空，直接返回");
-            return this.service;
-        }
-        WxMpProperties.MpConfig config1 = new WxMpProperties.MpConfig();
-        config1.setAppId(SystemConfig.getCS_AppSettings().get("MP.APPID").toString());
-        config1.setSecret(SystemConfig.getCS_AppSettings().get("MP.APPSECRET").toString());
-        config1.setToken(SystemConfig.getCS_AppSettings().get("MP.TOKEN").toString());
-        config1.setAesKey(SystemConfig.getCS_AppSettings().get("MP.EncodingAESKey").toString());
-        List<WxMpProperties.MpConfig> configs = new ArrayList<WxMpProperties.MpConfig>();
-        configs.add(config1);
+        if(this.newRouter==null){
+            log.info("service为空，创建单例");
+            WxMpProperties.MpConfig config1 = new WxMpProperties.MpConfig();
+            config1.setAppId(SystemConfig.getCS_AppSettings().get("MP.APPID").toString());
+            config1.setSecret(SystemConfig.getCS_AppSettings().get("MP.APPSECRET").toString());
+            config1.setToken(SystemConfig.getCS_AppSettings().get("MP.TOKEN").toString());
+            config1.setAesKey(SystemConfig.getCS_AppSettings().get("MP.EncodingAESKey").toString());
+            List<WxMpProperties.MpConfig> configs = new ArrayList<WxMpProperties.MpConfig>();
+            configs.add(config1);
 
-        if (configs == null) {
-            throw new RuntimeException("大哥，拜托先看下项目首页的说明（readme文件），添加下相关配置，注意别配错了！");
-        }
+            if (configs == null) {
+                throw new RuntimeException("大哥，拜托先看下项目首页的说明（readme文件），添加下相关配置，注意别配错了！");
+            }
 
-        WxMpService service = new WxMpServiceImpl();
-        service.setMultiConfigStorages(configs
-            .stream().map(a -> {
-                WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
-                configStorage.setAppId(a.getAppId());
-                configStorage.setSecret(a.getSecret());
-                configStorage.setAccessToken(MpUtil.getAccessTokenByInterface().getAccessToken());
-                configStorage.setToken(a.getToken());
-                configStorage.setAesKey(a.getAesKey());
-                return configStorage;
-            }).collect(Collectors.toMap(WxMpDefaultConfigImpl::getAppId, a -> a, (o, n) -> o)));
-        return service;
+            WxMpService service = new WxMpServiceImpl();
+            service.setMultiConfigStorages(configs
+                    .stream().map(a -> {
+                        WxMpDefaultConfigImpl configStorage = new WxMpDefaultConfigImpl();
+                        configStorage.setAppId(a.getAppId());
+                        configStorage.setSecret(a.getSecret());
+                        configStorage.setAccessToken(MpUtil.getAccessTokenByInterface().getAccessToken());
+                        configStorage.setToken(a.getToken());
+                        configStorage.setAesKey(a.getAesKey());
+                        return configStorage;
+                    }).collect(Collectors.toMap(WxMpDefaultConfigImpl::getAppId, a -> a, (o, n) -> o)));
+            this.service = service;
+        }
+        return this.service;
     }
 
     public synchronized WxMpMessageRouter messageRouter(WxMpService wxMpService) {
         //单例模式
-        if(this.newRouter!=null){
-            log.info("newRouter不为空，直接返回");
-            return this.newRouter;
+        if(this.newRouter==null){
+            log.info("newRouter为空，创建单例");
+            final WxMpMessageRouter newRouter = new WxMpMessageRouter(wxMpService);
+
+            // 记录所有事件的日志 （异步执行）
+            newRouter.rule().handler(this.logHandler).next();
+
+            // 接收客服会话管理事件
+            newRouter.rule().async(false).msgType(EVENT).event(KF_CREATE_SESSION)
+                    .handler(this.kfSessionHandler).end();
+            newRouter.rule().async(false).msgType(EVENT).event(KF_CLOSE_SESSION)
+                    .handler(this.kfSessionHandler).end();
+            newRouter.rule().async(false).msgType(EVENT).event(KF_SWITCH_SESSION)
+                    .handler(this.kfSessionHandler).end();
+
+            // 门店审核事件
+            newRouter.rule().async(false).msgType(EVENT).event(POI_CHECK_NOTIFY).handler(this.storeCheckNotifyHandler).end();
+
+            // 自定义菜单事件
+            newRouter.rule().async(false).msgType(EVENT).event(EventType.CLICK).handler(this.menuHandler).end();
+
+            // 点击菜单连接事件
+            newRouter.rule().async(false).msgType(EVENT).event(EventType.VIEW).handler(this.nullHandler).end();
+
+            // 关注事件
+            newRouter.rule().async(false).msgType(EVENT).event(SUBSCRIBE).handler(this.subscribeHandler).end();
+
+            // 取消关注事件
+            newRouter.rule().async(false).msgType(EVENT).event(UNSUBSCRIBE).handler(this.unsubscribeHandler).end();
+
+            // 上报地理位置事件
+            newRouter.rule().async(false).msgType(EVENT).event(EventType.LOCATION).handler(this.locationHandler).end();
+
+            // 接收地理位置消息
+            newRouter.rule().async(false).msgType(XmlMsgType.LOCATION).handler(this.locationHandler).end();
+
+            // 扫码事件
+            newRouter.rule().async(false).msgType(EVENT).event(EventType.SCAN).handler(this.scanHandler).end();
+
+            // 默认
+            newRouter.rule().async(false).handler(this.msgHandler).end();
+
+            this.newRouter = newRouter;
         }
-        final WxMpMessageRouter newRouter = new WxMpMessageRouter(wxMpService);
-
-        // 记录所有事件的日志 （异步执行）
-        newRouter.rule().handler(this.logHandler).next();
-
-        // 接收客服会话管理事件
-        newRouter.rule().async(false).msgType(EVENT).event(KF_CREATE_SESSION)
-            .handler(this.kfSessionHandler).end();
-        newRouter.rule().async(false).msgType(EVENT).event(KF_CLOSE_SESSION)
-            .handler(this.kfSessionHandler).end();
-        newRouter.rule().async(false).msgType(EVENT).event(KF_SWITCH_SESSION)
-            .handler(this.kfSessionHandler).end();
-
-        // 门店审核事件
-        newRouter.rule().async(false).msgType(EVENT).event(POI_CHECK_NOTIFY).handler(this.storeCheckNotifyHandler).end();
-
-        // 自定义菜单事件
-        newRouter.rule().async(false).msgType(EVENT).event(EventType.CLICK).handler(this.menuHandler).end();
-
-        // 点击菜单连接事件
-        newRouter.rule().async(false).msgType(EVENT).event(EventType.VIEW).handler(this.nullHandler).end();
-
-        // 关注事件
-        newRouter.rule().async(false).msgType(EVENT).event(SUBSCRIBE).handler(this.subscribeHandler).end();
-
-        // 取消关注事件
-        newRouter.rule().async(false).msgType(EVENT).event(UNSUBSCRIBE).handler(this.unsubscribeHandler).end();
-
-        // 上报地理位置事件
-        newRouter.rule().async(false).msgType(EVENT).event(EventType.LOCATION).handler(this.locationHandler).end();
-
-        // 接收地理位置消息
-        newRouter.rule().async(false).msgType(XmlMsgType.LOCATION).handler(this.locationHandler).end();
-
-        // 扫码事件
-        newRouter.rule().async(false).msgType(EVENT).event(EventType.SCAN).handler(this.scanHandler).end();
-
-        // 默认
-        newRouter.rule().async(false).handler(this.msgHandler).end();
-
-        return newRouter;
+        return this.newRouter;
     }
-
 }
