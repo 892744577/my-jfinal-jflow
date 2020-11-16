f69f00ed5ec86a6b3276e414da200a528a098e2a
roman <892744577@qq.com>
2020-11-16 17:35:50 +0800

    bug:district参数不存在

--- src/main/resources/template/admin/wxcmsAccountFans.sql
+++ src/main/resources/template/admin/wxcmsAccountFans.sql
@@ -6,8 +6,9 @@
     SELECT a.* FROM wxcms_account_fans a where 1=1
     #if(search)
       and (a.province LIKE concat('%',#para(search),'%') or a.city LIKE concat('%',#para(search),'%')
-      or a.district LIKE concat('%',#para(search),'%') or a.nick_name like CONCAT('%',#para(search),'%'))
+      or a.nick_name like CONCAT('%',#para(search),'%'))
     #end
+    order by a.create_time desc
   #end
   #sql ("delAccountFansByOpenId")
     delete FROM wxcms_account_fans where open_id = ?
