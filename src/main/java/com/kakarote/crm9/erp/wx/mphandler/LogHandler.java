10669d7d5cd146deed33132ff369d9be3935602b
roman <892744577@qq.com>
2020-11-16 17:47:53 +0800

    note:不用每一个消息都打印

--- src/main/java/com/kakarote/crm9/erp/wx/mphandler/LogHandler.java
+++ src/main/java/com/kakarote/crm9/erp/wx/mphandler/LogHandler.java
@@ -1,6 +1,5 @@
 package com.kakarote.crm9.erp.wx.mphandler;
 
-import com.kakarote.crm9.erp.wx.util.JsonUtils;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
@@ -16,7 +15,7 @@ public class LogHandler extends AbstractHandler {
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
                                     Map<String, Object> context, WxMpService wxMpService,
                                     WxSessionManager sessionManager) {
-        this.logger.info("\n接收到请求消息，内容：{}", JsonUtils.toJson(wxMessage));
+        //this.logger.info("\n接收到请求消息，内容：{}", JsonUtils.toJson(wxMessage));
         return null;
     }
 
