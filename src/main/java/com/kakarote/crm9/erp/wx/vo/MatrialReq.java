ebc1f6bd74fb7b309da931ca227982dfb92fb46f
roman <892744577@qq.com>
2020-11-27 14:06:09 +0800

    feat:多媒体数据上传

---
+++ src/main/java/com/kakarote/crm9/erp/wx/vo/MatrialReq.java
@@ -0,0 +1,9 @@
+package com.kakarote.crm9.erp.wx.vo;
+
+import lombok.Data;
+
+@Data
+public class MatrialReq {
+    private int i;
+    private int j;
+}
---
+++ src/main/java/com/kakarote/crm9/erp/wx/vo/WxMpMaterialReq.java
@@ -0,0 +1,8 @@
+package com.kakarote.crm9.erp.wx.vo;
+
+import lombok.Data;
+
+@Data
+public class WxMpMaterialReq {
+    private String name;
+}
