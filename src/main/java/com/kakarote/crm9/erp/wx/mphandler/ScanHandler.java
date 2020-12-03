8de11c50366f8feb802a021317f98823399bd14b
IT-KD <kidda_wang@163.com>
2020-12-03 17:53:43 +0800

    扫码事件代码提交

--- src/main/java/com/kakarote/crm9/erp/wx/mphandler/ScanHandler.java
+++ src/main/java/com/kakarote/crm9/erp/wx/mphandler/ScanHandler.java
@@ -1,5 +1,7 @@
 package com.kakarote.crm9.erp.wx.mphandler;
 
+import com.jfinal.aop.Inject;
+import com.kakarote.crm9.erp.wx.service.HandlerService;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -12,11 +14,15 @@ import java.util.Map;
  * @author Binary Wang(https://github.com/binarywang)
  */
 public class ScanHandler extends AbstractHandler {
+    @Inject
+    private HandlerService handlerService;
 
     @Override
-    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map,
+    public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> map,
                                     WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
+        //组装卡券数据
+        WxMpXmlOutMessage wxMpXmlOutMessage = handlerService.getWxMpXmlOutMessage(wxMessage,wxMpService);
         // 扫码事件处理
-        return null;
+        return handlerService.outMessage(wxMpXmlOutMessage);
     }
 }
