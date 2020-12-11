7008da122c96192b957712f149686c1ddeae0181
roman <892744577@qq.com>
2020-12-11 09:55:32 +0800

    feat:增加字段

--- src/main/resources/template/admin/hrGongDanRepair.sql
+++ src/main/resources/template/admin/hrGongDanRepair.sql
@@ -5,7 +5,9 @@
   #sql ("queryPageList")
     select * from (
     select a.*,(case when a.deal='2' then '2' when (SELECT COUNT(*)>0 FROM hr_gongdan_log t1
-    WHERE t1.preServiceNo=a.orderNumber)=1 then '1' else '0' end) deal_1 from hr_gongdan_repair a) b where 1=1
+    WHERE t1.preServiceNo=a.orderNumber)=1 then '1' else '0' end) deal_1,
+    (select count(*) from hr_gongdan_log c where c.preServiceNo = a.orderNumber) logNum from
+    hr_gongdan_repair a) b where 1=1
     #if(search)
       and (b.orderNumber like CONCAT('%',#para(search),'%') or b.contact like CONCAT('%',#para(search),'%') or b.equipSNCode like CONCAT('%',#para(search),'%')
       or b.phone like CONCAT('%',#para(search),'%') or b.address like CONCAT('%',#para(search),'%') or b.remark like CONCAT('%',#para(search),'%'))
