cfc24aa4ea32d0fd4cec0d5c9b84db64dd9b7704
roman <892744577@qq.com>
2021-01-11 11:15:09 +0800

    jxc:进销存功能完善

--- src/main/resources/template/admin/jxcOrderDeliveryCode.sql
+++ src/main/resources/template/admin/jxcOrderDeliveryCode.sql
@@ -2,8 +2,10 @@
   #sql ("getCodeByDocno")
     select * from jxc_order_delivery_code a where a.docno=#para(docno)
   #end
-
   #sql ("getByCode")
     select * from jxc_order_delivery_code a where a.code=#para(code)
+  #end
+  #sql ("getByCodeAndCustomer")
+    select * from jxc_order_delivery_code a where a.code=#para(code) and a.customer=#para(customer)
   #end
 #end
