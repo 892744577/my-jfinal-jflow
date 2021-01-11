6d32c5d856790be5d3fbd42b4ae630e090ed287c
IT-KD <kidda_wang@163.com>
2021-01-11 15:50:29 +0800

    旺店通信息接口代码提交

--- src/main/java/com/kakarote/crm9/erp/yeyx/controller/WdtController.java
+++ src/main/java/com/kakarote/crm9/erp/yeyx/controller/WdtController.java
@@ -16,11 +16,12 @@ public class WdtController extends Controller {
     public void getOrder() throws Exception{
 //        renderJson(R.ok().put("data",wdtService.gatewayRequest("http://qimen.api.taobao.com/router/qmtest",wdtService.getQmJsonData(new HashMap()))));
         Map map = new HashMap();
-        map.put("start_time", "2018-12-11 12:00:00");
-        map.put("end_time", "2018-12-11 13:00:00");
+        map.put("start_time", "2018-11-11 12:00:00");
+        map.put("end_time", "2018-11-11 13:00:00");
 //        map.put("start_time", DateUtil.changeStrToDate("2019-11-11 19:00:00"));
 //        map.put("end_time", DateUtil.changeStrToDate("2019-11-11 20:00:00"));
-        String result = wdtService.gatewayRequest("https://sandbox.wangdian.cn/openapi2/trade_query.php",wdtService.getJsonData(map));
+//        String result = wdtService.gatewayRequest("https://sandbox.wangdian.cn/openapi2/trade_query.php",wdtService.getJsonData(map));
+        String result = wdtService.gatewayRequest("https://api.wangdian.cn/openapi2/trade_query.php",wdtService.getJsonData(map));
         renderJson(R.ok().put("data",result));
 //        renderJson(R.ok().put("data",wdtService.gatewayRequestJson("https://sandbox.wangdian.cn/openapi2/trade_query.php",wdtService.getJsonData(map))));
     }
