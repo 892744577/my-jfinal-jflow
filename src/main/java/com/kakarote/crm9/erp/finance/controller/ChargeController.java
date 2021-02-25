6821f560b21c88955beee5e37736cc3a19213a93
roman <892744577@qq.com>
2021-02-25 10:23:04 +0800

    feat:充值

---
+++ src/main/java/com/kakarote/crm9/erp/finance/controller/ChargeController.java
@@ -0,0 +1,10 @@
+package com.kakarote.crm9.erp.finance.controller;
+
+import com.jfinal.core.Controller;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ChargeController extends Controller {
+
+
+}
---
+++ src/main/java/com/kakarote/crm9/erp/finance/cron/ChargeCron.java
@@ -0,0 +1,12 @@
+package com.kakarote.crm9.erp.finance.cron;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class ChargeCron implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
