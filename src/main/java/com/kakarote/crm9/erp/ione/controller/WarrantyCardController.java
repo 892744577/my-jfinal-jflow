1dfb94e69504c654bdfc003e3a5ed568fd1ec6d8
roman <892744577@qq.com>
2021-03-17 14:00:11 +0800

    feat:售后安装和保修卡

---
+++ src/main/java/com/kakarote/crm9/erp/ione/controller/WarrantyCardController.java
@@ -0,0 +1,9 @@
+package com.kakarote.crm9.erp.ione.controller;
+
+
+import com.jfinal.core.Controller;
+
+public class WarrantyCardController extends Controller {
+
+
+}
---
+++ src/main/java/com/kakarote/crm9/erp/jxc/controller/JxcAftermarketController.java
@@ -0,0 +1,12 @@
+package com.kakarote.crm9.erp.jxc.controller;
+
+import com.jfinal.core.Controller;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JxcAftermarketController extends Controller {
+
+
+
+
+}
