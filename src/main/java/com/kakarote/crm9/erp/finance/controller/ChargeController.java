0ba97d8910bc825158e7f378963702a607c8e35c
882744577 <892744577@qq.com>
2021-03-18 16:27:23 +0800

    feat:产品报备

--- src/main/java/com/kakarote/crm9/erp/finance/controller/ChargeController.java
+++ src/main/java/com/kakarote/crm9/erp/finance/controller/ChargeController.java
@@ -2,6 +2,8 @@ package com.kakarote.crm9.erp.finance.controller;
 
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
+import com.jfinal.core.paragetter.Para;
+import com.kakarote.crm9.erp.finance.entity.vo.CallT100ChargeApi6Request;
 import com.kakarote.crm9.erp.finance.service.ChargeService;
 import com.kakarote.crm9.utils.R;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,15 @@ public class ChargeController extends Controller {
 
     @Inject
     private ChargeService chargeService;
+
+    public void callT100ChargeApi6(@Para("") CallT100ChargeApi6Request callT100ChargeApi6Request) throws Exception {
+        String detail = callT100ChargeApi6Request.getDetailListJson();
+        renderJson(R.ok().put("data",chargeService.callT100ChargeApi6(
+                callT100ChargeApi6Request.getCustomerNo(),
+                callT100ChargeApi6Request.getNo(),
+                callT100ChargeApi6Request.getNum(),
+                null)));
+    }
 
     public void callT100ChargeApi7() throws Exception {
         renderJson(R.ok().put("data",chargeService.callT100ChargeApi7()));
